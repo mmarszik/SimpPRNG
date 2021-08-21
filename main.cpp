9624c4d11ccde15b978e951ea72fa0b4cc23e534
mmarszik <mmarszik@gmail.com>
2021-08-21 17:17:07 +0200

    .

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@
 #include <climits>
 #include <cstring>
 
-template<typename TBASE,typename TRET=unsigned int>
+template<typename TBASE, typename TRET=unsigned int, int BITS=sizeof(TRET)*CHAR_BIT >
 class SimpPRNG {
+    static_assert( sizeof(TBASE) >= sizeof(TRET)   , "sizeof(TBASE) >= sizeof(TRET)"   );
+    static_assert( BITS <= sizeof(TRET) * CHAR_BIT , "BITS <= sizeof(TRET) * CHAR_BIT" );
+
 private:
     TBASE x;
 
 public:
     SimpPRNG(const TBASE seed = 2569018830187087903ull) {
         x = seed;
-        for( int i=0 ; i<30 ; i++ ) {
+        for( int i=0 ; i<100 ; i++ ) {
             (*this)();
         }
     }
     TRET operator()() {
-        return ( x = (x * 79908886691ull + 8143076871913ull) ) >> ( (sizeof(TBASE) - sizeof(TRET) ) * CHAR_BIT );
+        return ( x = (x * 79908886691ull + 8143076871913ull) ) >> ( sizeof(TBASE) * CHAR_BIT - BITS );
     }
 };
 
@@ -37,14 +40,16 @@ int main( int argc , char *argv[] ) {
         size = strtoull(argv[2],&end,10);
     }
     typedef unsigned int type_out;
-    SimpPRNG<__uint128_t, type_out> r(seed);
+    typedef __uint128_t type_base;
+    constexpr int bits = 5;
+    SimpPRNG<type_base, type_out, bits> r(seed);
     constexpr int BUFF_SIZE = (1<<12);
     char buff[BUFF_SIZE];
     for( unsigned long long i=0 ; i<size ; ) {
         int j=0;
         while( j<BUFF_SIZE && i<size ) {
             type_out v = r();
-            for( int k=0 ; k<sizeof(type_out)*CHAR_BIT && j<BUFF_SIZE && i<size ; k++, j++, i++ ) {
+            for( int k=0 ; k<bits && j<BUFF_SIZE && i<size ; k++, j++, i++ ) {
                 buff[j] = '0' + (v & 1);
                 v >>= 1;
             }
